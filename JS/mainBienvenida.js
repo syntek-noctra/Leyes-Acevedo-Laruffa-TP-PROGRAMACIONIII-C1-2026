2b8e197be36021e8ac4bb2cdd1b688418a517b50
LucasFranco <being11011@gmail.com>
2026-05-02 02:10:18 -0300

    Corrección JS

--- JS/mainBienvenida.js
+++ JS/mainBienvenida.js
@@ -1,4 +1,6 @@
-const registerBtn=document.querySelector(".button-start");
+/* VARIABLES PRINCIPALES*/
+
+const registerBtn=document.querySelector(".button-enter");
 
 /**
  * @type {HTMLInputElement}
@@ -6,7 +8,16 @@ const registerBtn=document.querySelector(".button-start");
  */
 const userInput=document.getElementById("user-input");
 
+let hayError=false;
 
+const MIN_LOGIN = 4;
+
+const KEY_USER_LOCAL = "userLocal"
+/**
+ * 
+ * @param {string} mensaje  Mensaje de error brindado por el sistema.
+ * @returns {HTMLParagraphElement} texto de error
+ */
 function createError(mensaje){
     const p=document.createElement("p");
     p.classList.add("error");
@@ -14,17 +25,20 @@ function createError(mensaje){
     return p;
 }
 
+    
 
 registerBtn.addEventListener("click",(ev)=>{
     console.log("ASDDAS");
     ev.preventDefault();
-    let hayError=false;
+    
     
     const userName=userInput.value;
 
-    if(userName.length<4){
+    if(userName.length< MIN_LOGIN && !hayError){
         const p=createError("invalid name")
         userInput.parentElement.appendChild(p);
+        p.style.position = "fixed"
+        p.style.left = "1055px"
         hayError=true;
     }
 
@@ -33,7 +47,8 @@ registerBtn.addEventListener("click",(ev)=>{
 
     const user={name:userName}
 
-    localStorage.setItem("userLocal",JSON.stringify(user));
+    localStorage.setItem(KEY_USER_LOCAL,JSON.stringify(user));
+    window.location.href = "productos.html";
 
     
 });
